5b8f6f6f22d178ed6e11c4444f2d2e86adf66a86
santinocazzuli <santinocazzuli@gmail.com>
2026-06-15 20:59:08 -0300

    Update Funciones.py

--- Funciones.py
+++ Funciones.py
@@ -1,87 +1,109 @@
-def calcular_total_votos(votos_partidos: list) -> int:
+def calcular_total_votos(lista: list) -> int:
     total = 0
-    for voto in votos_partidos:
-        total += voto
+    for votos in lista:
+        total += votos
     return total
 
-def calcular_promedio_votos(votos_partidos: list) -> float:
-    total_votos = calcular_total_votos(votos_partidos)
-    cantidad_partidos = len(votos_partidos)
-    if cantidad_partidos == 0:
+def calcular_promedio_votos(lista: list) -> float:
+    total = calcular_total_votos(lista)
+    if len(lista) == 0:
         return 0.0
-    return total_votos / cantidad_partidos
+    return total / len(lista)
 
-def encontrar_menor_voto(votos_partidos: list) -> int:
-    menor = votos_partidos[0]
-    for voto in votos_partidos:
-        if voto < menor:
-            menor = voto
+def encontrar_menor_voto(lista: list) -> int:
+    menor = lista[0]
+    for votos in lista:
+        if votos < menor:
+            menor = votos
     return menor
 
-def encontrar_mayor_voto(votos_partidos: list) -> int:
-    mayor = votos_partidos[0]
-    for voto in votos_partidos:
-        if voto > mayor:
-            mayor = voto
+def encontrar_mayor_voto(lista: list) -> int:
+    mayor = lista[0]
+    for votos in lista:
+        if votos > mayor:
+            mayor = votos
     return mayor
 
-def filtrar_por_debajo_porcentaje(votos_partidos: list, limite_porcentaje: float) -> list:
-    total_votos = calcular_total_votos(votos_partidos)
-    partidos_filtrados = []
-    votos_filtrados = []
-    porcentajes_filtrados = []
+def filtrar_por_debajo_porcentaje(votos: list, limite: float) -> tuple:
+    total_votos = calcular_total_votos(votos)
+    cantidad_cumplen = 0
+    for i in range(len(votos)):
+        porcentaje = (votos[i] / total_votos) * 100
+        if porcentaje < limite:
+            cantidad_cumplen += 1
+    partidos_filtrados = [0] * cantidad_cumplen
+    votos_filtrados = [0] * cantidad_cumplen
+    porcentajes_filtrados = [0.0] * cantidad_cumplen
     porcentaje_acumulado = 0.0
-    for i in range(len(votos_partidos)):
-        porcentaje = (votos_partidos[i] / total_votos) * 100
-        if porcentaje < limite_porcentaje:
-            partidos_filtrados.append(i + 1)
-            votos_filtrados.append(votos_partidos[i])
-            porcentajes_filtrados.append(porcentaje)
+    indice = 0
+    for i in range(len(votos)):
+        porcentaje = (votos[i] / total_votos) * 100
+        if porcentaje < limite:
+            partidos_filtrados[indice] = i + 1
+            votos_filtrados[indice] = votos[i]
+            porcentajes_filtrados[indice] = porcentaje
             porcentaje_acumulado += porcentaje
-    return [partidos_filtrados, votos_filtrados, porcentajes_filtrados, porcentaje_acumulado]
+            indice += 1
+            
+    return (partidos_filtrados, votos_filtrados, porcentajes_filtrados, porcentaje_acumulado)
 
-def filtrar_por_encima_votos(votos_partidos: list, minimo_votos: int) -> list:
-    total_votos = calcular_total_votos(votos_partidos)
-    partidos_filtrados = []
-    votos_filtrados = []
-    porcentajes_filtrados = []
-    for i in range(len(votos_partidos)):
-        if votos_partidos[i] > minimo_votos:
-            partidos_filtrados.append(i + 1)
-            votos_filtrados.append(votos_partidos[i])
-            porcentajes_filtrados.append((votos_partidos[i] / total_votos) * 100)
-    return [partidos_filtrados, votos_filtrados, porcentajes_filtrados]
+def filtrar_por_encima_votos(votos: list, limite_votos: int) -> tuple:
+    total_votos = calcular_total_votos(votos)
+    cantidad_cumplen = 0
+    for i in range(len(votos)):
+        if votos[i] > limite_votos:
+            cantidad_cumplen += 1
+    partidos_filtrados = [0] * cantidad_cumplen
+    votos_filtrados = [0] * cantidad_cumplen
+    porcentajes_filtrados = [0.0] * cantidad_cumplen
+    indice = 0
+    for i in range(len(votos)):
+        if votos[i] > limite_votos:
+            porcentaje = (votos[i] / total_votos) * 100
+            partidos_filtrados[indice] = i + 1
+            votos_filtrados[indice] = votos[i]
+            porcentajes_filtrados[indice] = porcentaje
+            indice += 1
+            
+    return (partidos_filtrados, votos_filtrados, porcentajes_filtrados)
 
-def obtener_partidos_menos_votados(votos_partidos: list) -> list:
-    menor_voto = encontrar_menor_voto(votos_partidos)
-    total_votos = calcular_total_votos(votos_partidos)
-    partidos_indices = []
-    partidos_votos = []
-    partidos_porcentajes = []
-    for i in range(len(votos_partidos)):
-        if votos_partidos[i] == menor_voto:
-            partidos_indices.append(i + 1)
-            partidos_votos.append(votos_partidos[i])
-            partidos_porcentajes.append((votos_partidos[i] / total_votos) * 100)
-    return [partidos_indices, partidos_votos, partidos_porcentajes]
+def obtener_partidos_menos_votados(votos: list) -> tuple:
+    total_votos = calcular_total_votos(votos)
+    menor_voto = encontrar_menor_voto(votos)
+    cantidad_minimos = 0
+    for v in votos:
+        if v == menor_voto:
+            cantidad_minimos += 1
+    partidos_filtrados = [0] * cantidad_minimos
+    votos_filtrados = [0] * cantidad_minimos
+    porcentajes_filtrados = [0.0] * cantidad_minimos
+    indice = 0
+    for i in range(len(votos)):
+        if votos[i] == menor_voto:
+            partidos_filtrados[indice] = i + 1
+            votos_filtrados[indice] = votos[i]
+            porcentajes_filtrados[indice] = (votos[i] / total_votos) * 100
+            indice += 1         
+    return (partidos_filtrados, votos_filtrados, porcentajes_filtrados)
 
-def evaluar_segunda_vuelta_logica(votos_partidos: list) -> list:
-    total_votos = calcular_total_votos(votos_partidos)
-    mayor_voto = encontrar_mayor_voto(votos_partidos)
-    id_ganador = -1
-    for i in range(len(votos_partidos)):
-        if votos_partidos[i] == mayor_voto:
-            id_ganador = i + 1
-            break
+def evaluar_segunda_vuelta_logica(votos: list) -> list:
+    total_votos = calcular_total_votos(votos)
+    mayor_voto = encontrar_mayor_voto(votos) 
+    ganador_id = 0
+    for i in range(len(votos)):
+        if votos[i] == mayor_voto:
+            ganador_id = i + 1
+            break      
     porcentaje_ganador = (mayor_voto / total_votos) * 100
     if porcentaje_ganador <= 50.0:
-        return [True, id_ganador, mayor_voto, porcentaje_ganador]
-    return [False, id_ganador, mayor_voto, porcentaje_ganador]
+        return [True, ganador_id, mayor_voto, porcentaje_ganador]
+    else:
+        return [False, ganador_id, mayor_voto, porcentaje_ganador]
 
 def ordenar_nombres_alfabeticamente(lista_nombres: list) -> list:
-    nombres_ordenados = []
-    for nombre in lista_nombres:
-        nombres_ordenados.append(nombre)
+    nombres_ordenados = [0] * len(lista_nombres)
+    for i in range(len(lista_nombres)):
+        nombres_ordenados[i] = lista_nombres[i]   
     limite = len(nombres_ordenados)
     for i in range(limite - 1):
         for j in range(0, limite - i - 1):
@@ -89,4 +111,5 @@ def ordenar_nombres_alfabeticamente(lista_nombres: list) -> list:
                 auxiliar = nombres_ordenados[j]
                 nombres_ordenados[j] = nombres_ordenados[j + 1]
                 nombres_ordenados[j + 1] = auxiliar
+                
     return nombres_ordenados
